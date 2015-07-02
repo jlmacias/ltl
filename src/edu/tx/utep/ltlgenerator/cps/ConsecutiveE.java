9535435a6e9eacb9f2655550b86943dd68e31326
ostrovskis <elvijsostrovskis@gmail.com>
2015-07-02 09:27:04 -0600

    Adding ConsecutiveE Logic

--- src/edu/tx/utep/ltlgenerator/cps/ConsecutiveE.java
+++ src/edu/tx/utep/ltlgenerator/cps/ConsecutiveE.java
@@ -6,7 +6,40 @@ public class ConsecutiveE extends CompositeProposition {
 
 	@Override
 	public String generateLTL(String letter, int count) {
-		return "";
+		StringBuilder formula = new StringBuilder();
+		StringBuilder intermediateString = new StringBuilder();
+		StringBuilder initialString = new StringBuilder();
+
+		// Initial String (Not Repetitive)
+		initialString.append("(");
+		for (int i = 1; i <= count; i++) {
+			initialString.append("!").append(letter).append(i);
+
+			if (i < count) {
+				initialString.append(" ^ ");
+			}
+		}
+		initialString.append(") ");
+
+		// Repetitive Pattern
+		for (int i = 1; i < count; i++) {
+			intermediateString.append("(").append(letter).append(i).append(" ^ ").append(generateProposition(NOT_AND, letter, i+1, count));
+
+			if (i+1 < count) {
+				intermediateString.append(" ^ X");
+			} else {
+				intermediateString.append(" ^ X").append(letter).append(i+1);
+			}
+		}
+
+		// Adding Parenthesis At The End
+		for (int i = 1; i < count; i++) {
+			intermediateString.append(")");
+		}
+
+		formula.append(initialString).append("^ (").append(initialString).append("U ").append(intermediateString).append(")");
+
+		return formula.toString();
 	}
 
 }
