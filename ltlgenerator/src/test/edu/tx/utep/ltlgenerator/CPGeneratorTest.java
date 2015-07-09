85b355666da42e797e4d1a9269b4e27e6fe77c51
ostrovskis <elvijsostrovskis@gmail.com>
2015-07-08 23:02:02 -0600

    Updating testing file.

--- ltlgenerator/src/test/edu/tx/utep/ltlgenerator/CPGeneratorTest.java
+++ ltlgenerator/src/test/edu/tx/utep/ltlgenerator/CPGeneratorTest.java
@@ -93,6 +93,4 @@ public class CPGeneratorTest {
 		String output = cpGenerator.getCompositeProposition("L_EventualE_4");
 		assertEquals("(!l1 ^ !l2 ^ !l3 ^ !l4) ^ ((!l1 ^ !l2 ^ !l3 ^ !l4) U (l1 ^ !l2 ^ !l3 ^ !l4 ^ ((!l2 ^ !l3 ^ !l4) U (l2 ^ !l3 ^ !l4 ^ ((!l3 ^ !l4) U (l3 ^ !l4 ^ (!l4 U l4)))))))", output);
 	}
-	
-	
 }
