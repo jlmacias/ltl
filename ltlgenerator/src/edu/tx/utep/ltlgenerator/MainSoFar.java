1d02a134344632664ef94cf3b5fe30ba737556a8
jennifer <jennifer.weand@gmail.com>
2015-07-13 11:53:23 -0600

    Updated main to run the JUnit tests

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/MainSoFar.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/MainSoFar.java
@@ -1,22 +1,21 @@
 package edu.tx.utep.ltlgenerator;
 
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+
+import test.edu.tx.utep.ltlgenerator.ZTestSuite;
+
 public class MainSoFar {
 
-	private static CPGenerator cpGenerator = new CPGenerator();
-
 	public static void main(String[] args) {
-		System.out.println("AtLeastOneC: " + cpGenerator.getCompositeProposition("L_AtLeastOneC_4"));
-		System.out.println("AtLeastOneC: " + cpGenerator.getCompositeProposition("P_AtLeastOneH_5"));
-		System.out.println("AtLeastOneE: " + cpGenerator.getCompositeProposition("L_AtLeastOneE_3"));
-		System.out.println("ParallelC: " + cpGenerator.getCompositeProposition("R_ParallelC_4"));
-		System.out.println("ParallelH: " + cpGenerator.getCompositeProposition("Q_ParallelH_5"));
-		System.out.println("ParallelE: " + cpGenerator.getCompositeProposition("L_ParallelE_3"));
-		System.out.println("ConsecutiveC: " + cpGenerator.getCompositeProposition("Q_ConsecutiveC_4"));
-		System.out.println("ConsecutiveH: " + cpGenerator.getCompositeProposition("P_ConsecutiveH_5"));
-		System.out.println("ConsecutiveE: " + cpGenerator.getCompositeProposition("P_ConsecutiveE_4"));
-		System.out.println("EventualC: " + cpGenerator.getCompositeProposition("L_EventualC_3"));
-		System.out.println("EventualH: " + cpGenerator.getCompositeProposition("R_EventualH_4"));
-		System.out.println("EventualE: " + cpGenerator.getCompositeProposition("Q_EventualE_5"));
+		JUnitCore junit = new JUnitCore();
+		Result result = junit.run(ZTestSuite.class);
+		
+		int passed = result.getRunCount() - result.getFailureCount() - result.getIgnoreCount();
+		System.out.println("Ran " + result.getRunCount() + " tests");
+		System.out.println("\tFailed:  " + result.getFailureCount());
+		System.out.println("\tIgnored: " + result.getIgnoreCount());
+		System.out.println("\tPassed:  " + passed);
 	}
 
 }
