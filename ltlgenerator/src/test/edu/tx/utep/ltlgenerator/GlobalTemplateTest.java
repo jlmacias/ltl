6e3b59da25387d384bc87d69ab04ed7416292265
Juan Ochoa <jochoa105@gmail.com>
2015-07-24 17:42:12 -0600

    Update GlobalTemplateTest.java
    Added testGlobalQPrecedesPEPlus

--- ltlgenerator/src/test/edu/tx/utep/ltlgenerator/GlobalTemplateTest.java
+++ ltlgenerator/src/test/edu/tx/utep/ltlgenerator/GlobalTemplateTest.java
@@ -30,17 +30,20 @@ public class GlobalTemplateTest {
 	}
 	
 	/********/
+	// Q can be anything BUT AtLeastOneC or ParallelC, and P has to be conditional type
 	@Test
     	public void testGlobalQPrecedesPCPlus() throws Exception {
-        	String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPCPlus", "Q_ConsecutiveC_3", "P_AtLeastOneE_2", "","");
-        	assertEquals("!((!([q1 & [(!p1 ^ !p2) ^ ((!p1 ^ !p2) U (p1 v p2))]] ^ X([q2 & [(!p1 ^ !p2) ^ ((!p1 ^ !p2) U (p1 v p2))]] ^ X(q3)))) U ((!p1 ^ !p2) ^ ((!p1 ^ !p2) U (p1 v p2))))", output);
+        	String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPCPlus", "Q_ConsecutiveC_3", "P_ParallelC_3", "","");
+        	assertEquals("!((!([q1 & [(!p1 ^ !p2 ^ !p3)]] ^ X([q2 & [(!p1 ^ !p2 ^ !p3)]] ^ X(q3)))) U (p1 ^ p2 ^ p3))", output);
     	}
 
-	@Test
+    	// Q can be anything BUT AtLeastOneC or ParallelC, and P has to be event type
+    	@Test
     	public void testGlobalQPrecedesPEPlus() throws Exception {
         	String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPEPlus", "Q_ConsecutiveC_3", "P_AtLeastOneE_3", "","");
-        	assertEquals("!((!([q1 & [!(!p1 ^ !p2 ^ X(p1 ^ p2))]] ^ X([q2 & [!(!p1 ^ !p2 ^ X(p1 ^ p2))]] ^ X(q3)))) U (!p1 ^ !p2 ^ X(p1 ^ p2)))", output);
+        	assertEquals("!((!([q1 & [!(!p1 ^ !p2 ^ !p3 ^ X(p1 v p2 v p3))]] ^ X([q2 & [!(!p1 ^ !p2 ^ !p3 ^ X(p1 v p2 v p3))]] ^ X(q3)))) U (!p1 ^ !p2 ^ !p3 ^ X(p1 v p2 v p3)))", output);
     	}
+
 
 
 
