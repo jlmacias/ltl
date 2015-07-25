be0244ad4036689e5ac14b699a264ec8fb95a393
jennifer <jennifer.weand@gmail.com>
2015-07-25 13:32:50 -0600

    Fixed the &r test cases with the less verbose output

--- ltlgenerator/src/test/edu/tx/utep/ltlgenerator/AndRGeneratorTest.java
+++ ltlgenerator/src/test/edu/tx/utep/ltlgenerator/AndRGeneratorTest.java
@@ -22,7 +22,7 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_AtLeastOneC_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_AtLeastOneH_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(l1 v l2 v l3) & [(p1 v p2 v p3)]]", output);
+		assertEquals("[[(l1 v l2 v l3)] & [(p1 v p2 v p3)]]", output);
 	}
 
 	@Test
@@ -30,7 +30,7 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_AtLeastOneE_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_AtLeastOneH_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(!l1 ^ !l2 ^ !l3) & [(p1 v p2 v p3)]] ^ ([(!l1 ^ !l2 ^ !l3) & [(p1 v p2 v p3)]] U [(l1 v l2 v l3) & [(p1 v p2 v p3)]])", output);
+		assertEquals("[[(!l1 ^ !l2 ^ !l3) ^ ((!l1 ^ !l2 ^ !l3) U (l1 v l2 v l3))] & [(p1 v p2 v p3)]]", output);
 	}
 
 	@Test
@@ -38,7 +38,7 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_ParallelC_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelH_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(l1 ^ l2 ^ l3) & [(p1 ^ p2 ^ p3)]]", output);
+		assertEquals("[[(l1 ^ l2 ^ l3)] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
 	@Test
@@ -46,15 +46,15 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("R_ParallelE_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelH_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(!r1 ^ !r2 ^ !r3) & [(p1 ^ p2 ^ p3)]] ^ ([(!r1 ^ !r2 ^ !r3) & [(p1 ^ p2 ^ p3)]] U [(r1 ^ r2 ^ r3) & [(p1 ^ p2 ^ p3)]])", output);
+		assertEquals("[[(!r1 ^ !r2 ^ !r3) ^ ((!r1 ^ !r2 ^ !r3) U (r1 ^ r2 ^ r3))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
-	@Test
+	@Test//
 	public void testConsecutiveC() throws Exception {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_ConsecutiveC_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelH_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("([l1 & [(p1 ^ p2 ^ p3)]] ^ X([l2 & [(p1 ^ p2 ^ p3)]] ^ X([l3 & [(p1 ^ p2 ^ p3)]])))", output);
+		assertEquals("[[(l1 ^ X(l2 ^ X(l3)))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
 	@Test                   //need consecutive h array list code to test
@@ -62,7 +62,7 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_ConsecutiveH_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelC_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("([l1 ^ !l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ X([l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ X([l3 & [(p1 ^ p2 ^ p3)]])))", output);
+		assertEquals("[[(l1 ^ !l2 ^ !l3 ^ X(l2 ^ !l3 ^ X(l3)))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 	
 	@Test
@@ -70,32 +70,31 @@ public class AndRGeneratorTest {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_ConsecutiveE_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelC_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(!l1 ^ !l2 ^ !l3) & [(p1 ^ p2 ^ p3)]] ^ ([(!l1 ^ !l2 ^ !l3) & [(p1 ^ p2 ^ p3)]] U ([l1 ^ !l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ X([l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ X[(l3) & [(p1 ^ p2 ^ p3)]])))", output);
+		assertEquals("[[(!l1 ^ !l2 ^ !l3) ^ ((!l1 ^ !l2 ^ !l3) U (l1 ^ !l2 ^ !l3 ^ X(l2 ^ !l3 ^ X(l3))))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
-	@Test                    //need eventual c array list code to test
+	@Test
 	public void testEventualC() throws Exception {
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_EventualC_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelC_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("([l1 & [(p1 ^ p2 ^ p3)]] ^ X([!l2 & [(p1 ^ p2 ^ p3)]] U ([l2 & [(p1 ^ p2 ^ p3)]] ^ X([!l3 & [(p1 ^ p2 ^ p3)]] U ([l3 & [(p1 ^ p2 ^ p3)]])))))", output);
+		assertEquals("[[(l1 ^ X(!l2 U (l2 ^ X(!l3 U (l3)))))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
-	@Test                    //need eventual h array list code to test
+	@Test
 	public void testEventualH() throws Exception {         
 		List<String> leftSides = cpGenerator.getCompositeProposition("L_EventualH_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelC_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("([l1 ^ !l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ ([(!l2 ^ !l3) & [(p1 ^ p2 ^ p3)]] U ([l2 ^ !l3 & [(p1 ^ p2 ^ p3)]] ^ ([(!l3) & [(p1 ^ p2 ^ p3)]] U ([l3 & [(p1 ^ p2 ^ p3)]])))))", output);
+		assertEquals("[[(l1 ^ !l2 ^ !l3 ^ ((!l2 ^ !l3) U (l2 ^ !l3 ^ ((!l3) U (l3)))))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
-	@Test                   //need eventual e array list code to test
+	@Test
 	public void testEventualE() throws Exception {
 		List<String> leftSides = cpGenerator.getCompositeProposition("R_EventualE_3");
 		List<String> rightSides = cpGenerator.getCompositeProposition("P_ParallelC_3");
 		String output = operatorGenerator.getAndedPropositions(ANDR, leftSides, rightSides);
-		assertEquals("[(!r1 ^ !r2 ^ !r3) & [(p1 ^ p2 ^ p3)]] ^ ([(!r1 ^ !r2 ^ !r3) & [(p1 ^ p2 ^ p3)]] U ([r1 ^ !r2 ^ !r3 & [(p1 ^ p2 ^ p3)]] ^ ([(!r2 ^ !r3) & [(p1 ^ p2 ^ p3)]] U ([r2 ^ !r3 & [(p1 ^ p2 ^ p3)]] ^ ([(!r3) & [(p1 ^ p2 ^ p3)]] U ([r3 & [(p1 ^ p2 ^ p3)]]))))))", output);
-
+		assertEquals("[[(!r1 ^ !r2 ^ !r3) ^ ((!r1 ^ !r2 ^ !r3) U (r1 ^ !r2 ^ !r3 ^ ((!r2 ^ !r3) U (r2 ^ !r3 ^ ((!r3) U (r3))))))] & [(p1 ^ p2 ^ p3)]]", output);
 	}
 
 }
