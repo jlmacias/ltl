36f097acc31587cd822b39f8dd47f8cbc14b3ba9
jennifer <jennifer.weand@gmail.com>
2015-07-25 15:31:27 -0600

    AfterL Test cases update

--- ltlgenerator/src/test/edu/tx/utep/ltlgenerator/AfterLTemplateTest.java
+++ ltlgenerator/src/test/edu/tx/utep/ltlgenerator/AfterLTemplateTest.java
@@ -22,16 +22,43 @@ public class AfterLTemplateTest {
 	@Test
 	public void testExistanceOfPAfterL() throws Exception {
 		String output = ltlGenerator.generateLTLFormula("ExistanceOfPAfterL", "P_AtLeastOneC_4", "", "", "L_ConsecutiveC_3");
-		//(l1 ^ X(l2 ^ X(l3))) L_ConsecutiveC_3
-		// G!(p1 v p2 v p3 v p4) "GlobalAbsenceOfP", "P_AtLeastOneC_4"
-		// (l1 ^ X(l2 ^ X([l3 & [F(p1 v p2 v p3 v p4)]])))
 		assertEquals("!((!(l1 ^ X(l2 ^ X(l3)))) U (l1 ^ X(l2 ^ X([l3 & [!(F(p1 v p2 v p3 v p4))]]))))", output);
 	}
 	
+//	@Test
+//	public void testQStrictlyPrecedesPcAfterL() throws Exception {
+//		String output = ltlGenerator.generateLTLFormula("QStrictlyPrecedesPcAfterL", "P_AtLeastOneE_3", "Q_AtLeastOneH_3", "", "L_ConsecutiveC_3");
+//		assertEquals("", output);
+//	}
+	
 	@Test
-	public void testQRespondsToPAfterL() throws Exception {
-		String output = ltlGenerator.generateLTLFormula("QRespondsToPAfterL", "P_AtLeastOneE_3", "Q_AtLeastOneH_3", "", "L_ConsecutiveC_3");
-		assertEquals("!((!(l1 ^ X(l2 ^ X(l3)))) U (l1 ^ X(l2 ^ X([l3 & [!(G((!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3)) -> ((!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U [(p1 v p2 v p3) & [F((q1 v q2 v q3))]])))))]]))))", output);
+	public void testQStrictlyPrecedesPeAfterL() throws Exception {
+		String output = ltlGenerator.generateLTLFormula("QStrictlyPrecedesPeAfterL", "P_AtLeastOneE_3", "Q_AtLeastOneH_3", "", "L_ConsecutiveC_3");
+		assertEquals("!((!(l1 ^ X(l2 ^ X(l3)))) U (l1 ^ X(l2 ^ X([l3 & [!(!((!([[(q1 v q2 v q3)] & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]])) U ((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))))]]))))", output);
+	}
+	
+	@Test
+	public void testGlobalQPrecedesPcAfterLStar() throws Exception {
+		String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPcAfterL", "P_AtLeastOneE_3", "Q_AtLeastOneH_3", "", "L_ConsecutiveC_3");
+		assertEquals("!((!(l1 ^ X(l2 ^ X(l3)))) U (l1 ^ X(l2 ^ X([l3 & [!(!((!([(q1 v q2 v q3) & [!(!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))]])) U (!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))))]]))))", output);
+	}
+	
+	@Test
+	public void testGlobalQPrecedesPcAfterLPlus() throws Exception {
+		String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPcAfterL", "P_AtLeastOneE_3", "Q_ConsecutiveE_3", "", "L_ParallelC_3");
+		assertEquals("!((!(l1 ^ l2 ^ l3)) U [(l1 ^ l2 ^ l3) & [!(!((!([(!q1 ^ !q2 ^ !q3) & [!(!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))]] ^ ([(!q1 ^ !q2 ^ !q3) & [!(!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))]] U ([q1 ^ !q2 ^ !q3 & [!(!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))]] ^ X([q2 ^ !q3 & [!(!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))]] ^ X(q3)))))) U (!p1 ^ !p2 ^ !p3) ^ ((!p1 ^ !p2 ^ !p3) U (p1 v p2 v p3))))]])", output);
+	}
+	
+	@Test
+	public void testGlobalQPrecedesPeAfterLStar() throws Exception {
+		String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPeAfterL", "P_AtLeastOneE_3", "Q_AtLeastOneH_3", "", "L_ConsecutiveC_3");
+		assertEquals("!((!(l1 ^ X(l2 ^ X(l3)))) U (l1 ^ X(l2 ^ X([l3 & [!(!((![(q1 v q2 v q3) & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]]) U ((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))))]]))))", output);
+	}
+	
+	@Test
+	public void testGlobalQPrecedesPeAfterLPlus() throws Exception {
+		String output = ltlGenerator.generateLTLFormula("GlobalQPrecedesPeAfterL", "P_AtLeastOneE_3", "Q_ConsecutiveE_3", "", "L_ParallelC_3");
+		assertEquals("!((!(l1 ^ l2 ^ l3)) U [(l1 ^ l2 ^ l3) & [!(!((![(!q1 ^ !q2 ^ !q3) & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]] ^ ([(!q1 ^ !q2 ^ !q3) & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]] U ([q1 ^ !q2 ^ !q3 & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]] ^ X([q2 ^ !q3 & [!((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))]] ^ X(q3))))) U ((!p1 ^ !p2 ^ !p3) ^ X(p1 v p2 v p3))))]])", output);
 	}
 	
 }
