d206f97ccd3a928a987dd609e5d87ae3067c6420
jennifer <jennifer.weand@gmail.com>
2015-07-28 10:55:28 -0600

    Added the method to clean up the [, ], & from the operators.  Won't implement until the Templates are completed to keep the readablility for the test cases.

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/LTLGenerator.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/LTLGenerator.java
@@ -13,12 +13,21 @@ public class LTLGenerator {
 		
 			template = new TemplateFactory().getTemplate(templateName, qProposition);
 			String formula = template.generateFormula(pProposition, qProposition, rProposition, lProposition);
+			// TODO return the cleaned up string once we're ready to turn this in
+			//return replaceOperatorStrings(formula);
 			return formula;
 
 		} catch (TemplateNotFoundException e) {
 			e.printStackTrace();
 			return e.getMessage();
 		}
+	}
+	
+	private String replaceOperatorStrings(String formula) {
+		formula = formula.replace(OutputCharacters.O_AND, OutputCharacters.AND);
+		formula = formula.replace(OutputCharacters.O_CLOSE_P, OutputCharacters.CLOSE_P);
+		formula = formula.replace(OutputCharacters.O_OPEN_P, OutputCharacters.OPEN_P);
+		return formula;
 	}
 
 }
