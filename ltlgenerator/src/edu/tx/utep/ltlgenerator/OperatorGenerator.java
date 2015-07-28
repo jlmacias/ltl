2d869923a8cf30b181414e793fe9500cb559e462
jennifer <jennifer.weand@gmail.com>
2015-07-28 09:20:34 -0600

    fixed the output with the exception.

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/OperatorGenerator.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/OperatorGenerator.java
@@ -9,19 +9,18 @@ import edu.tx.utep.ltlgenerator.operators.Operator;
 public class OperatorGenerator {
 
 	public String getAndedPropositions(String andType, List<String> leftSides, List<String> rightSides) {
-		String right = OutputCharacters.O_OPEN_P + String.join("", rightSides) + OutputCharacters.O_CLOSE_P;
+		
 		Operator operator;
 		try {
+			
+			String right = OutputCharacters.O_OPEN_P + String.join("", rightSides) + OutputCharacters.O_CLOSE_P;
 			operator = new OperatorFactory().getOperatorClass(andType);
-
-			if (operator != null)
-				return operator.andCPs(leftSides, right);
+			return operator.andCPs(leftSides, right);
+			
 		} catch (OperatorNotFoundException e) {
 			e.printStackTrace();
+			return e.getMessage();
 		}
-
-		return "No Operator class of " + andType;
-
 	}
 
 }
