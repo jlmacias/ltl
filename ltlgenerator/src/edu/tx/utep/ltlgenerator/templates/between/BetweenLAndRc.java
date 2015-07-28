f01069409810af567a6bc1a81295e54a914b8fa9
ostrovskis <elvijsostrovskis@gmail.com>
2015-07-27 18:56:21 -0600

    Removing duplicate declaration in Between L and Rc

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/between/BetweenLAndRc.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/between/BetweenLAndRc.java
@@ -17,21 +17,22 @@ public class BetweenLAndRc extends Template {
     @Override
     public String generateFormula(String pProposition, String qProposition, String rProposition, String lProposition) {
         String formula = template;
+        List<String> l = getCompositeProposition(lProposition);
         String PRString = beforeRTemplate.generateFormula(pProposition, qProposition, rProposition, lProposition);
-        List<String> l1 = getCompositeProposition(lProposition);
-        List<String> l2 = getCompositeProposition(lProposition);
         List<String> r = getCompositeProposition(rProposition);
-        String lString = String.join("", l1);
+        String lString = String.join("", l);
         String rString = String.join("", r);
 
         List<String> notR = new ArrayList<String>();
         notR.add("!" + rString);
-        String andedL1 = operatorGenerator.getAndedPropositions(AND_L, l1, notR);
+        String andedL1 = operatorGenerator.getAndedPropositions(AND_L, l, notR);
         formula = formula.replace("(L &l !R)", andedL1);
+
+        l = getCompositeProposition(lProposition);
 
         List<String> PR = new ArrayList<>();
         PR.add(PRString);
-        String andedL2 = operatorGenerator.getAndedPropositions(AND_L, l2, PR);
+        String andedL2 = operatorGenerator.getAndedPropositions(AND_L, l, PR);
         formula = formula.replace("(L &l PR)", andedL2);
 
         formula = formula.replace("R", rString);
