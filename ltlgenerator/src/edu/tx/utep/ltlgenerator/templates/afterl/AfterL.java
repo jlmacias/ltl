f3952521a3ccb77ff75ec24f96eb3bf90c03fe0f
jennifer <jennifer.weand@gmail.com>
2015-07-28 16:31:13 -0600

    Refactored out magic strings for AfterL Template

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/afterl/AfterL.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/afterl/AfterL.java
@@ -3,11 +3,18 @@ package edu.tx.utep.ltlgenerator.templates.afterl;
 import java.util.ArrayList;
 import java.util.List;
 
+import edu.tx.utep.ltlgenerator.OutputCharacters;
 import edu.tx.utep.ltlgenerator.templates.Template;
 
 public class AfterL extends Template {
 
-	private String template = "!((!L) U (L &l !Pg))";
+	// L &l !Pg
+	private String lAndNotPg = "L &l " + OutputCharacters.NOT + "Pg";
+
+	// !((!L) U (L &l !Pg))
+	private String template = OutputCharacters.NOT + OutputCharacters.OPEN_P + OutputCharacters.OPEN_P + OutputCharacters.NOT + "L" + OutputCharacters.CLOSE_P + OutputCharacters.UNTIL +
+			OutputCharacters.OPEN_P + lAndNotPg + OutputCharacters.CLOSE_P + OutputCharacters.CLOSE_P;
+
 	private Template globalTemplate;
 	
 	public AfterL(Template globalTemplate){
@@ -22,12 +29,13 @@ public class AfterL extends Template {
 		String lString = String.join("", l);
 		
 		List<String> notP = new ArrayList<String>();
-		notP.add("!(");
+		notP.add(OutputCharacters.NOT);
+		notP.add(OutputCharacters.OPEN_P);
 		notP.add(pg);
-		notP.add(")");
+		notP.add(OutputCharacters.CLOSE_P);
 		
 		String andedP = operatorGenerator.getAndedPropositions(AND_L, l, notP);
-		formula = formula.replace("(L &l !Pg)", andedP);
+		formula = formula.replace(lAndNotPg, andedP);
 		
 		formula = formula.replace("L", lString);
 
--- ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/global/GlobalQPrecedesPEPlus.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/global/GlobalQPrecedesPEPlus.java
@@ -9,15 +9,15 @@ import edu.tx.utep.ltlgenerator.templates.Template;
 
 public class GlobalQPrecedesPEPlus extends Template {
 
-	// (pInverse ^ XPh)
-    private static String pInverseOrXPh = OutputCharacters.OPEN_P + "pInverse" + OutputCharacters.OR + OutputCharacters.NEXT + "Ph" + OutputCharacters.CLOSE_P;
+	// (Pi ^ XPh)
+    private static String piOrXPh = OutputCharacters.OPEN_P + "Pi" + OutputCharacters.OR + OutputCharacters.NEXT + "Ph" + OutputCharacters.CLOSE_P;
 
-	// Q &-l !(pInverse ^ XPh)
-    private static String qAndNotSomething = "Q &-l " + OutputCharacters.NOT + pInverseOrXPh;
+	// Q &-l !(Pi ^ XPh)
+    private static String qAndNotSomething = "Q &-l " + OutputCharacters.NOT + piOrXPh;
 
-    // !((!(Q &-l !(pInverse ^ XPh))) U (pInverse ^ XPh))
+    // !((!(Q &-l !(Pi ^ XPh))) U (Pi ^ XPh))
     private static String template = OutputCharacters.NOT + OutputCharacters.OPEN_P + OutputCharacters.OPEN_P + OutputCharacters.NOT + OutputCharacters.OPEN_P +
-    		qAndNotSomething + OutputCharacters.CLOSE_P + OutputCharacters.CLOSE_P + OutputCharacters.UNTIL + pInverseOrXPh + OutputCharacters.CLOSE_P;
+    		qAndNotSomething + OutputCharacters.CLOSE_P + OutputCharacters.CLOSE_P + OutputCharacters.UNTIL + piOrXPh + OutputCharacters.CLOSE_P;
 
     @Override
     public String generateFormula(String pProposition, String qProposition, String rProposition, String lProposition) {
@@ -27,11 +27,11 @@ public class GlobalQPrecedesPEPlus extends Template {
 		String pInverse = String.join("", getParallelInverse(pProposition));
 
         List<String> rightSide = new ArrayList<String>();
-        rightSide.add(OutputCharacters.NOT + pInverseOrXPh);
+        rightSide.add(OutputCharacters.NOT + piOrXPh);
         formula = formula.replace(qAndNotSomething, operatorGenerator.getAndedPropositions(AND_MINUS_L, q, rightSide));
 
         formula = formula.replace("Ph", String.join("", pH));
-        formula = formula.replace("pInverse", pInverse);
+        formula = formula.replace("Pi", pInverse);
 
         return formula;
     }
