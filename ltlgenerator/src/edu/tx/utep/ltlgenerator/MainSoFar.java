5c9dc365468e16cf7544579ca8a3fff4aa70d504
ostrovskis <elvijsostrovskis@gmail.com>
2015-07-31 18:38:42 -0600

    Updating Between L and Rc to the latest syntax.

--- ltlgenerator/src/edu/tx/utep/ltlgenerator/MainSoFar.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/MainSoFar.java
@@ -7,31 +7,28 @@ public class MainSoFar {
 		LTLGenerator ltlGenerator = new LTLGenerator();
 
 		// Between L and Rc
-		System.out.println("1. " + ltlGenerator.generateLTLFormula("AbsenceOfPBetweenLAndRc", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("2. " + ltlGenerator.generateLTLFormula("ExistenceOfPBetweenLAndRc", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("3. " + ltlGenerator.generateLTLFormula("QPrecedesPcBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelC_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("4. " + ltlGenerator.generateLTLFormula("QPrecedesPeBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("5. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPcBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("6. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPeBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("7. " + ltlGenerator.generateLTLFormula("QRespondsToPBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("1. " + ltlGenerator.generateLTLFormula("AbsenceOfPBetweenLAndRc", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("2. " + ltlGenerator.generateLTLFormula("ExistenceOfPBetweenLAndRc", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("3. " + ltlGenerator.generateLTLFormula("QPrecedesPcBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelC_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("4. " + ltlGenerator.generateLTLFormula("QPrecedesPeBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("5. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPcBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("6. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPeBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("7. " + ltlGenerator.generateLTLFormula("QRespondsToPBetweenLAndRc", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
 
-		System.out.println();
+//		System.out.println();
 
 		// Between L and Re
-		System.out.println("1. " + ltlGenerator.generateLTLFormula("AbsenceOfPBetweenLAndRe", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("2. " + ltlGenerator.generateLTLFormula("ExistenceOfPBetweenLAndRe", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("3. " + ltlGenerator.generateLTLFormula("QPrecedesPcBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelC_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("4. " + ltlGenerator.generateLTLFormula("QPrecedesPeBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("5. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPcBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("6. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPeBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
-		System.out.println("7. " + ltlGenerator.generateLTLFormula("QRespondsToPBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("1. " + ltlGenerator.generateLTLFormula("AbsenceOfPBetweenLAndRe", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("2. " + ltlGenerator.generateLTLFormula("ExistenceOfPBetweenLAndRe", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("3. " + ltlGenerator.generateLTLFormula("QPrecedesPcBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelC_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("4. " + ltlGenerator.generateLTLFormula("QPrecedesPeBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("5. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPcBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("6. " + ltlGenerator.generateLTLFormula("QStrictlyPrecedesPeBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
+//		System.out.println("7. " + ltlGenerator.generateLTLFormula("QRespondsToPBetweenLAndRe", "P_AtLeastOneC_3", "Q_ParallelE_3", "R_ParallelC_3", "L_ConsecutiveC_3"));
 
-		System.out.println();
-
+//		System.out.println();
 		System.out.println(ltlGenerator.generateLTLFormula("QRespondsToPBetweenLAndRc", "P_ConsecutiveC_2", "Q_ParallelC_2", "R_AtLeastOneC_2", "L_ParallelC_2"));
 
-		// AfterLUntilRc
-//		System.out.println("1. " + ltlGenerator.generateLTLFormula("AbsenceOfPBetweenLAndRe", "P_AtLeastOneC_3", "", "R_ParallelC_3", "L_ConsecutiveC_3"));
 	}
 
 }
--- ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/between/BetweenLAndRc.java
+++ ltlgenerator/src/edu/tx/utep/ltlgenerator/templates/between/BetweenLAndRc.java
@@ -1,5 +1,6 @@
 package edu.tx.utep.ltlgenerator.templates.between;
 
+import edu.tx.utep.ltlgenerator.OutputCharacters;
 import edu.tx.utep.ltlgenerator.templates.Template;
 
 import java.util.ArrayList;
@@ -7,7 +8,19 @@ import java.util.List;
 
 public class BetweenLAndRc extends Template {
 
-    private String template = "G((L &l !R) -> (L &l PR))";
+    // L &r !R
+    private static String L_AndL_NotR = "L &l " + OutputCharacters.NOT + "R";
+
+    // L &l PR
+    private static String L_AndL_PR = "L &l PR";
+
+    // G((L &l !R) -> (L &l PR))
+    private static String template =
+            OutputCharacters.ALWAYS +
+            OutputCharacters.OPEN_P + OutputCharacters.OPEN_P + L_AndL_NotR + OutputCharacters.CLOSE_P +
+            OutputCharacters.IMPLY +
+            OutputCharacters.OPEN_P + L_AndL_PR + OutputCharacters.CLOSE_P + OutputCharacters.CLOSE_P;
+
     private Template beforeRTemplate;
 
     public BetweenLAndRc(Template beforeRTemplate){
@@ -18,25 +31,23 @@ public class BetweenLAndRc extends Template {
     public String generateFormula(String pProposition, String qProposition, String rProposition, String lProposition) {
         String formula = template;
         List<String> l = getCompositeProposition(lProposition);
-        String PRString = beforeRTemplate.generateFormula(pProposition, qProposition, rProposition, lProposition);
         List<String> r = getCompositeProposition(rProposition);
+        String PRString = beforeRTemplate.generateFormula(pProposition, qProposition, rProposition, lProposition);
         String lString = String.join("", l);
         String rString = String.join("", r);
 
         List<String> notR = new ArrayList<String>();
-        notR.add("!" + rString);
+        notR.add(0, OutputCharacters.NOT);
+
         String andedL1 = operatorGenerator.getAndedPropositions(AND_L, l, notR);
-        formula = formula.replace("(L &l !R)", andedL1);
+        formula = formula.replace(L_AndL_NotR, andedL1);
 
         l = getCompositeProposition(lProposition);
 
         List<String> PR = new ArrayList<>();
         PR.add(PRString);
         String andedL2 = operatorGenerator.getAndedPropositions(AND_L, l, PR);
-        formula = formula.replace("(L &l PR)", andedL2);
-
-        formula = formula.replace("R", rString);
-        formula = formula.replace("L", lString);
+        formula = formula.replace(L_AndL_PR, andedL2);
 
         return formula;
     }
